17ce0f0e358075b9242105bcc7fb62b39556bff3
KBbitsP <75751774+KBbitsP@users.noreply.github.com>
2024-01-26 17:41:19 +0530

    Test

--- demo/src/pages/recorder/RecorderActionsPage.js
+++ demo/src/pages/recorder/RecorderActionsPage.js
@@ -1,66 +1,167 @@
-import React, { Component } from 'react';
-import { ZiggeoRecorder } from 'react-ziggeo';
-import { API_KEY } from '../../constants';
+import React, { Component } from "react";
+import { ZiggeoRecorder, ZiggeoAudioRecorder } from "react-ziggeo";
+import { API_KEY } from "../../constants";
 
-export default class RecorderActionsPage extends Component {
+export default class Recorder extends Component {
+  constructor(props) {
+    super(props);
+    this.child = false;
+    this.state = {
+      recording: false,
+      showError: false,
+      showWarning: false,
+      showRecorder: true,
+    };
+  }
 
-    static recorderInstance = null;
-    static recorderProperties = null;
+  recorderVerifiedVideo = () => {
+    console.log("Recorder onRecorderVerified");
+    alert("The video is verified, you can close the screen");
+    this.child.reset();
+  };
 
-    constructor (props) {
-        super(props);
-        this.state = {
-            apiKey: API_KEY,
-            recorder: {
-                height: 240,
-                width: 320
-            }
-        }
+  recorderNoMicrophone = () => {
+    alert("Please check sound");
+  };
+
+  recorderVerifiedAudio = () => {
+    console.log("Recorder onRecorderVerified");
+    alert("The video is verified, you can close the screen");
+    this.child.reset();
+  };
+
+  render() {
+    const {
+      title,
+      timerValue,
+      classes,
+      video = true,
+      width = "100%",
+      height = "auto",
+
+      allowupload,
+      recordingTimeLimit = 0,
+      className = "",
+      onRecordingStopped,
+    } = this.props;
+    const { showError, showWarning, showRecorder } = this.state;
+    const recordingTimeLimitInSec = recordingTimeLimit * 60 || 600;
+    let timeLimit = timerValue || recordingTimeLimitInSec;
+    timeLimit =
+      timeLimit < recordingTimeLimitInSec ? timeLimit : recordingTimeLimitInSec;
+
+    const testButton = (
+      <>
+        <button
+          onClick={() => {
+            this.setState({ showRecorder: false });
+          }}
+        >
+          Hide
+        </button>
+        <button
+          onClick={() => {
+            this.setState({ showRecorder: true });
+          }}
+        >
+          UnHide
+        </button>
+        <button
+          onClick={() => {
+            this.child.reset();
+          }}
+        >
+          Reset
+        </button>
+      </>
+    );
+    if (!showRecorder) {
+      return testButton;
     }
 
-    componentDidMount () {
-        this.recorderInstance = this.child.recorderInstance();
-        this.recorderProperties = this.recorderInstance.get();
-        console.log('ins :: ', this.recorderInstance, this.recorderProperties);
-    }
+    return (
+      <>
+        <div>Recorder</div>
+        {showError && (
+          <div className={clsx(classes.banner, classes.errorBanner)}>
+            No microphone found
+          </div>
+        )}
+        {showWarning && (
+          <div className={clsx(classes.banner, classes.warningBanner)}>
+            The microphone level is low
+          </div>
+        )}
+        {video ? (
+          <ZiggeoRecorder
+            apiKey={API_KEY}
+            onRef={(ref) => {
+              this.child = ref;
+            }}
+            title={title || ""}
+            description=""
+            height={height}
+            width={width}
+            theme="space"
+            themecolor="blue"
+            className={className}
+            localplayback={false}
+            picksnapshots={false}
+            timelimit={timeLimit}
+            allowupload={allowupload}
+            allowedextensions={[
+              "mp4",
+              "avi",
+              "mov",
+              "mkv",
+              "wmv",
+              "ogv",
+              "webm",
+              "flv",
+            ]}
+            // webrtc
+            webrtc_on_mobile
+            webrtc_streaming_if_necessary
+            // Events
 
-    setNewDimensions = () => {
-        let recorder = Object.assign({}, this.state.recorder);
-        recorder.width = 440;
-        recorder.height = 330;
-        this.setState({ recorder });
-    }
-
-    getInstance = () => {
-    }
-
-    render() {
-        return (
-            <section className="recorder-page">
-                <h1 className="page-header">Recorder With Actions Page</h1>
-                <p className="alert alert-warning">
-                    <strong>Note: </strong>
-                    It's demonstrative view, recording and uploading set to not allowed, you can register <a href="https://ziggeo.com">Ziggeo</a>. After getting key, use all awesome features
-                </p>
-                <ZiggeoRecorder
-                    apiKey={this.state.apiKey}
-                    onRef={ref => (this.child = ref)}
-                    height={this.state.recorder.height}
-                    width={this.state.recorder.width}
-                />
-
-                <div className="buttons">
-                    <button className="btn btn-xs btn-success" onClick={this.setNewDimensions}>Set Dimensions to 440x330</button>
-                </div>
-                <div className="detalis">
-                    { (this.state.recorder.height && this.state.recorder.width) &&
-                        <div>
-                            <span>Height: {this.state.recorder.height}</span>
-                            <span>Width: {this.state.recorder.width}</span>
-                        </div>
-                    }
-                </div>
-            </section>
-        );
-    }
+            onVerified={this.recorderVerifiedVideo}
+            onNoMicrophone={this.recorderNoMicrophone}
+          />
+        ) : (
+          <ZiggeoAudioRecorder
+            apiKey={API_KEY}
+            onRef={(ref) => {
+              this.child = ref;
+            }}
+            title={title || ""}
+            description=""
+            height={height}
+            width={width}
+            theme="space"
+            themecolor="blue"
+            className={className}
+            localplayback={false}
+            timelimit={timeLimit}
+            allowupload={allowupload}
+            allowedextensions={["mp3", "wav"]}
+            // webrtc
+            webrtc_on_mobile
+            webrtc_streaming_if_necessary
+            // Events
+            onCountdown={this.recorderCountdown}
+            onRecording={this.onRecording}
+            onUploadSelected={this.recorderUploadSelected}
+            onUploading={this.uploadStarted}
+            onVerified={this.recorderVerifiedAudio}
+            onError={this.onError}
+            onPlaying={this.onRecordPlaying}
+            onNoMicrophone={this.onNoMicrophone}
+            onHasMicrophone={this.onHasMicrophone}
+            onMicrophonehealth={this.onMicrophonehealth}
+          />
+        )}
+        {testButton}
+      </>
+    );
+  }
 }
